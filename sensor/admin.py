81c0a493968d58fc36a02cb6fa66a9560852903d
xp-rodion <only.learning.ariy@gmail.com>
2023-03-07 12:04:11 +0300

    feat: redact admin model

--- sensor/admin.py
+++ sensor/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from sensor.models import Event, Sensor
 
-admin.site.register(Event)
-admin.site.register(Sensor)
+
+@admin.register(Event)
+class EventAdminModel(admin.ModelAdmin):
+    fields = ('sensor_id', 'name', 'temperature', 'humidity')
+    list_display = ('name', )
+
+
+@admin.register(Sensor)
+class SensorAdminModel(admin.ModelAdmin):
+    fields = ('name', 'type')
+    list_display = ('name', )
