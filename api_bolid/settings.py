e34e90217882bbfb51541b39ae6e965512e02492
xp-rodion <only.learning.ariy@gmail.com>
2023-03-07 13:01:56 +0300

    feat: add .env variables

--- api_bolid/settings.py
+++ api_bolid/settings.py
@@ -89,11 +89,11 @@ WSGI_APPLICATION = 'api_bolid.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "postgres_password",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
+        "HOST": env("POSTGRES_HOST"),
+        "PORT": env("POSTGRES_PORT"),
     }
 }
 
